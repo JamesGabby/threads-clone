694a54449fae701d0de1b9f3a84eb593fa809e8d
JamesGabby <jlghfe36@gmail.com>
2023-08-19 17:50:28 +0100

    create bar components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import '../globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from "@clerk/nextjs"
+import Topbar from '@/components/shared/Topbar'
+import Bottombar from '@/components/shared/Bottombar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
