54e17c92562ca1a5e405788fac9152acb7d0bb79
JamesGabby <jlghfe36@gmail.com>
2023-08-19 17:43:28 +0100

    create structure of home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,7 +15,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {children}
+          <Topbar />
+
+          <main>
+            <LeftSidebar />
+
+            <section className='main-container'>
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
