0ed45ad2081febc95fab63424e2b6b731b2f9917
JamesGabby <jlghfe36@gmail.com>
2023-08-20 21:58:43 +0100

    fix logo and add sign out to left sidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -4,6 +4,7 @@ import { sidebarLinks } from '@/constants'
 import Link from 'next/link'
 import Image from 'next/image'
 import { usePathname, useRouter } from 'next/navigation'
+import { SignOutButton, SignedIn } from '@clerk/nextjs'
 
 function LeftSidebar() {
   const router = useRouter()
@@ -23,6 +24,21 @@ function LeftSidebar() {
             </Link>
           )}
         )}
+      </div>
+
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton>
+            <div className='flex cursor-pointer'>
+              <Image
+                src='/assets/logout.svg'
+                alt='logout'
+                width={24}
+                height={24}
+              />
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   )
