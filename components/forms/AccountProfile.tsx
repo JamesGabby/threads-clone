33a90cc4653575502d74e6d92fc3fa98b634bedd
JamesGabby <jlghfe36@gmail.com>
2023-08-22 00:39:50 +0100

    add utils functions

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,6 +18,7 @@ import * as z from "zod"
 import { UserValidation } from '@/lib/validations/user'
 import Image from "next/image"
 import { ChangeEvent, useState } from "react"
+import { isBase64Image } from "@/lib/utils"
 
 interface Props {
   user: {
@@ -67,9 +68,9 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
   }
 
   function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+    const blob = values.profile_photo
+
+    const hasImageChanged = isBase64Image(blob)
   }
 
   return (
