45e7555bc7d25f7c273094933289897bfb373d8c
JamesGabby <jlghfe36@gmail.com>
2023-08-23 21:27:40 +0100

    correct  type, update mb limit, add update reroute

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -94,6 +94,12 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
       userId: user.id,
       path: pathname
     })
+
+    if (pathname === '/profile/edit') {
+      router.back()
+    } else {
+      router.push('/')
+    }
   }
 
   return (
--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   experimental: {
     serverActions: true,
+    serverActionsBodySizeLimit: '20mb',
     serverComponentsExternalPackages: ["mongoose"],
   },
   eslint: {
