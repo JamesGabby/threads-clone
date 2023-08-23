19c893b70c6a91eeb10f677387155f0fdf38d0e5
JamesGabby <jlghfe36@gmail.com>
2023-08-23 04:55:57 +0100

    add values for updating user, refactor params

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import Image from "next/image"
 import { ChangeEvent, useState } from "react"
 import { isBase64Image } from "@/lib/utils"
 import { useUploadThing } from '@/lib/uploadthing'
+import { usePathname, useRouter } from "next/navigation"
+import { updateUser } from "@/lib/actions/user.actions"
 
 interface Props {
   user: {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([])
   const { startUpload } = useUploadThing('media')
+  const router = useRouter()
+  const pathname = usePathname()
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -81,6 +85,15 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         values.profile_photo = imgRes[0].fileUrl
       }
     }
+
+    await updateUser({
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      userId: user.id,
+      path: pathname
+    })
   }
 
   return (
