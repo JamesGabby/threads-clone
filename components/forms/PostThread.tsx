ebcd1b325ce313df3af2936d075a5b22fe168f92
JamesGabby <jlghfe36@gmail.com>
2023-08-25 07:20:00 +0100

    build structure for posting thread form

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -14,13 +14,10 @@ import { Textarea } from "@/components/ui/textarea"
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from "zod"
-import { UserValidation } from '@/lib/validations/user'
-import Image from "next/image"
-import { ChangeEvent, useState } from "react"
-import { isBase64Image } from "@/lib/utils"
-import { useUploadThing } from '@/lib/uploadthing'
 import { usePathname, useRouter } from "next/navigation"
+
 import { updateUser } from "@/lib/actions/user.actions"
+import { ThreadValidation } from '@/lib/validations/thread'
 
 interface Props {
   user: {
@@ -35,18 +32,14 @@ interface Props {
 }
 
 function PostThread({ userId }: { userId: string }) {
-  const [files, setFiles] = useState<File[]>([])
-  const { startUpload } = useUploadThing('media')
   const router = useRouter()
   const pathname = usePathname()
 
   const form = useForm({
-    resolver: zodResolver(UserValidation),
+    resolver: zodResolver(ThreadValidation),
     defaultValues: {
-      profile_photo: user?.image || "",
-      name: user?.name || "",
-      username: user?.username || "",
-      bio: user?.bio || ""
+      thread: '',
+      accountId: userId
     }
   })
 
