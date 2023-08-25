6076cba885e079aee25610b75d3687ce365675d0
JamesGabby <jlghfe36@gmail.com>
2023-08-25 07:12:20 +0100

    add <FormMessage />comps to AccountProfile

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -126,6 +126,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   onChange={(e) => handleImage(e, field.onChange)} 
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -145,6 +146,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -164,6 +166,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -183,6 +186,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
                   {...field}
                 />
               </FormControl>
+              <FormMessage />
             </FormItem>
           )}
         />
--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,5 +1,58 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Textarea } from "@/components/ui/textarea"
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from "zod"
+import { UserValidation } from '@/lib/validations/user'
+import Image from "next/image"
+import { ChangeEvent, useState } from "react"
+import { isBase64Image } from "@/lib/utils"
+import { useUploadThing } from '@/lib/uploadthing'
+import { usePathname, useRouter } from "next/navigation"
+import { updateUser } from "@/lib/actions/user.actions"
+
+interface Props {
+  user: {
+    id: string,
+    objectId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string
+  },
+  btnTitle: string
+}
+
 function PostThread({ userId }: { userId: string }) {
-  return <h1>Post Thread Form</h1>
+  const [files, setFiles] = useState<File[]>([])
+  const { startUpload } = useUploadThing('media')
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const form = useForm({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || ""
+    }
+  })
+
+  return (
+    <h1>Post Thread Form</h1>
+  )
 }
 
 export default PostThread
