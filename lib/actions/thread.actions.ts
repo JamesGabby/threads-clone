93a34a88cc65e9bd4a21f0bf7ff72ee65f2a2bdb
JamesGabby <jlghfe36@gmail.com>
2023-08-25 21:26:00 +0100

    create thread actions

---
+++ lib/actions/thread.actions.ts
@@ -0,0 +1,18 @@
+import { connectToDB } from "../mongoose"
+
+interface Params {
+  text: string
+  author: string
+  communityId: string
+  path: string
+}
+
+export async function createThread({ text, author, communityId, path }: Params) {
+  try {
+    connectToDB()
+
+    const createThread = await Thread.create()
+  } catch (error: any) {
+    throw new Error(`Failed to create thread: ${error.message}`)
+  }
+}
