e815a151ba495a8cfe4ecccc988ae6f4e23704a3
JamesGabby <jlghfe36@gmail.com>
2023-08-29 08:43:53 +0100

    pass props into ThreadCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,25 @@ export default async function Home() {
     <>
       <h1 className="head-text text-left">Home</h1>
       <section className="mt-9 flex flex-col gap-10">
-        {}
+        {result.threads.length === 0 ? (
+          <p className="no-result">No threads found</p>
+        ) : (
+          <>
+            {result.threads.map((thread) => (
+              <ThreadCard 
+                key={thread._id}
+                id={thread._id}
+                currentUserId={user?.id}
+                parentId={thread.parentId}
+                content={thread.text}
+                author={thread.author}
+                community={thread.community}
+                createdAt={thread.createdAt}
+                comments={thread.children}
+              />
+            ))}
+          </>
+        )}
       </section>
     </>
   )
