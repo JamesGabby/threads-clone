7fd3e93e9c592ce9b1562e3c808b93b77758ac26
JamesGabby <jlghfe36@gmail.com>
2023-08-29 07:14:00 +0100

    implement fetch threads function

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -31,3 +31,35 @@ export async function createThread({ text, author, communityId, path }: Params) 
     throw new Error(`Failed to create thread: ${error.message}`)
   }
 }
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB()
+
+    const skipAmount = (pageNumber - 1) * pageSize
+
+    const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: 'desc' })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({ path: 'author', model: User })
+      .populate({
+        path: 'children',
+        populate: {
+          path: 'author',
+          model: User,
+          select: "_id name parentId image"
+        }
+      })
+
+      const totalThreadsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } })
+
+      const threads = await threadsQuery.exec()
+
+      const isNext = totalThreadsCount > skipAmount + threads.length
+
+      return {threads, isNext}
+  } catch (error: any) {
+    throw new Error(`Failed to fetch threads: ${error.message}`)
+  }
+}
