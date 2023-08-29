2eaea27c05828e96572135f9a0df5299310951ee
JamesGabby <jlghfe36@gmail.com>
2023-08-29 07:19:40 +0100

    start implementing fetch threads on home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import { fetchThreads } from "@/lib/actions/thread.actions"
+import { currentUser } from "@clerk/nextjs"
 
 export default async function Home() {
-  const result = await fetchThreads()
+  const result = await fetchThreads(1, 30)
+  const user = await currentUser()
 
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
+      <section className="mt-9 flex flex-col gap-10">
+        {}
+      </section>
     </>
   )
 }
