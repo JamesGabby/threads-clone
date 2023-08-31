b87e24ae7d06816d12a048e5ad5c358385035035
JamesGabby <jlghfe36@gmail.com>
2023-09-01 00:55:38 +0100

    start building out Comment form

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -1,9 +1,9 @@
 import ThreadCard from '@/components/cards/ThreadCard'
+import Comment from '@/components/forms/Comment'
 import { fetchThreadById } from '@/lib/actions/thread.actions'
 import { fetchUser } from '@/lib/actions/user.actions'
 import { currentUser } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
-import React from 'react'
 
 const Page = async ({ params }: { params: { id: string } }) => {
   if (!params.id) return null
@@ -30,6 +30,10 @@ const Page = async ({ params }: { params: { id: string } }) => {
           createdAt={thread.createdAt}
           comments={thread.children}
         />
+      </div>
+
+      <div className="mt-7">
+        <Comment />
       </div>
     </section>
   )
