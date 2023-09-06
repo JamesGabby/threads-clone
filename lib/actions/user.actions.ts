788fc5ed599122897ff829d40c8b73ad2ef7280d
JamesGabby <jlghfe36@gmail.com>
2023-09-06 11:15:05 +0100

    start bulding out activity ui and fetch activities

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -24,7 +24,7 @@ export async function updateUser({
   image,
 }: Params): Promise<void> {
   try {
-    connectToDB();
+    connectToDB()
 
     await User.findOneAndUpdate(
       { id: userId },
@@ -36,13 +36,13 @@ export async function updateUser({
         onboarded: true,
       },
       { upsert: true }
-    );
+    )
 
     if (path === "/profile/edit") {
-      revalidatePath(path);
+      revalidatePath(path)
     }
   } catch (error: any) {
-    throw new Error(`Failed to create/update user: ${error.message}`);
+    throw new Error(`Failed to create/update user: ${error.message}`)
   }
 }
 
@@ -89,68 +89,68 @@ export async function fetchUsers({
   pageSize = 20,
   sortBy = "desc",
 }: {
-  userId: string;
-  searchString?: string;
-  pageNumber?: number;
-  pageSize?: number;
-  sortBy?: SortOrder;
+  userId: string
+  searchString?: string
+  pageNumber?: number
+  pageSize?: number
+  sortBy?: SortOrder
 }) {
   try {
-    connectToDB();
+    connectToDB()
 
     // Calculate the number of users to skip based on the page number and page size.
-    const skipAmount = (pageNumber - 1) * pageSize;
+    const skipAmount = (pageNumber - 1) * pageSize
 
     // Create a case-insensitive regular expression for the provided search string.
-    const regex = new RegExp(searchString, "i");
+    const regex = new RegExp(searchString, "i")
 
     // Create an initial query object to filter users.
     const query: FilterQuery<typeof User> = {
       id: { $ne: userId }, // Exclude the current user from the results.
-    };
+    }
 
     // If the search string is not empty, add the $or operator to match either username or name fields.
     if (searchString.trim() !== "") {
       query.$or = [
         { username: { $regex: regex } },
         { name: { $regex: regex } },
-      ];
+      ]
     }
 
     // Define the sort options for the fetched users based on createdAt field and provided sort order.
-    const sortOptions = { createdAt: sortBy };
+    const sortOptions = { createdAt: sortBy }
 
     const usersQuery = User.find(query)
       .sort(sortOptions)
       .skip(skipAmount)
-      .limit(pageSize);
+      .limit(pageSize)
 
     // Count the total number of users that match the search criteria (without pagination).
-    const totalUsersCount = await User.countDocuments(query);
+    const totalUsersCount = await User.countDocuments(query)
 
-    const users = await usersQuery.exec();
+    const users = await usersQuery.exec()
 
     // Check if there are more users beyond the current page.
-    const isNext = totalUsersCount > skipAmount + users.length;
+    const isNext = totalUsersCount > skipAmount + users.length
 
-    return { users, isNext };
+    return { users, isNext }
   } catch (error) {
-    console.error("Error fetching users:", error);
-    throw error;
+    console.error("Error fetching users:", error)
+    throw error
   }
 }
 
 export async function getActivity(userId: string) {
   try {
-    connectToDB();
+    connectToDB()
 
     // Find all threads created by the user
-    const userThreads = await Thread.find({ author: userId });
+    const userThreads = await Thread.find({ author: userId })
 
     // Collect all the child thread ids (replies) from the 'children' field of each user thread
     const childThreadIds = userThreads.reduce((acc, userThread) => {
-      return acc.concat(userThread.children);
-    }, []);
+      return acc.concat(userThread.children)
+    }, [])
 
     // Find and return the child threads (replies) excluding the ones created by the same user
     const replies = await Thread.find({
@@ -160,11 +160,11 @@ export async function getActivity(userId: string) {
       path: "author",
       model: User,
       select: "name image _id",
-    });
+    })
 
-    return replies;
+    return replies
   } catch (error) {
-    console.error("Error fetching replies: ", error);
-    throw error;
+    console.error("Error fetching replies: ", error)
+    throw error
   }
 }
